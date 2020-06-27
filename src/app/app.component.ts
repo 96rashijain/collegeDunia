883d0ad4d413d780a989cbf73a4b95e988764f9f
96rashijain <96rashijain@gmail.com>
2020-06-27 10:59:19 +0530

    ui complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { HttpClient } from "@angular/common/http";
 import * as data from "./colleges.json";
 import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
 // import {  } from "@fortawesome/free-solid-svg-icons";;
-import { faStar } from "@fortawesome/free-regular-svg-icons";
+// import { faStar } from "@fortawesome/free-regular-svg-icons";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faTag } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: "app-root",
@@ -13,9 +15,33 @@ import { faStar } from "@fortawesome/free-regular-svg-icons";
 export class AppComponent {
   collegeData = data.default.colleges;
   filmIcon = faStar;
+  tagIcon = faTag;
 
   constructor(private httpClient: HttpClient) {}
   ngOnInit() {
     console.log(this.collegeData);
+    var counter = 0;
+    $(window).scroll(function () {
+      if (
+        $(window).scrollTop() == $(document).height() - $(window).height() &&
+        counter < 2
+      ) {
+        appendData();
+      }
+    });
+    function appendData() {
+      var html = "";
+      for (var i = 0; i < 10; i++) {
+        html +=
+          '<p class="dynamic">Dynamic Data :  This is test data.<br />Next line.</p>';
+      }
+      $("#myScroll").append(html);
+      counter++;
+
+      if (counter == 2)
+        $("#myScroll").append(
+          '<button id="uniqueButton" style="margin-left: 50%; background-color: powderblue;">Click</button><br /><br />'
+        );
+    }
   }
 }
